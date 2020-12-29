9b20e07a110baacccf36808ce9a72b2afed20f1b
soniharshil54 <sharshil43@yahoo.com>
2020-12-29 22:38:19 +0530

    nr: add pseudocode in push

--- Linkedlist/Singly_Linked_List_Push.js
+++ Linkedlist/Singly_Linked_List_Push.js
@@ -1,6 +1,20 @@
 // piece of data - val
 //reference to next node - next
 
+/* ==============Pushing pseudocode================== */
+// This function should accept a value
+
+// Create a new node using the value passed to the function
+
+// If there is no head property on the list, set the head and tail to be the newly created node
+
+// Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+
+// Incretement the length by one
+
+// Return the linked list
+/* ==================================================== */
+
 class Node{
     constructor(val){
         this.val = val;
