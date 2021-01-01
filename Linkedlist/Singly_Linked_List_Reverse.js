b4417161156b8b2944621895af3309015e44f8ed
soniharshil54 <sharshil43@yahoo.com>
2021-01-01 23:24:46 +0530

    nr: Singly Linked List Reverse added

---
+++ Linkedlist/Singly_Linked_List_Reverse.js
@@ -0,0 +1,176 @@
+// piece of data - val
+//reference to next node - next
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    pop(){
+        if(!this.head){
+            return undefined
+        }
+        let current = this.head
+        let newTail = current
+        while(current.next){
+            newTail = current
+            current = current.next
+        }
+        this.tail = newTail
+        this.tail.next = null
+        this.length--
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return current
+    }
+    shift(){
+        if(!this.head){
+            return undefined
+        }
+        let currentHead = this.head
+        this.head = currentHead.next
+        this.length--
+        if(this.length === 0){
+            this.tail = null
+        }
+        return currentHead
+    }
+    unshift(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }
+        else{
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+    get(index){
+        if(index < 0 || index >= this.length){
+            return null
+        }
+        let counter = 0
+        let currentHead = this.head
+        while (counter < index) {
+            currentHead = currentHead.next
+            counter++
+        }
+        return currentHead
+    }
+    set(index, val){
+        let node = this.get(index)
+        if(!node){
+            return false
+        }
+        node.val  = val
+        return true 
+    }
+    insert(index, val){
+        if(index < 0 || index > this.length){
+            return false
+        }
+        else if(index === this.length){
+            !!this.push(val)
+        }
+        else if(index === 0){
+            !!this.unshift(val)  
+        }
+        else{
+            let prevNode = this.get(index-1)
+            let nextNode = prevNode.next
+            let newNode = new Node(val)
+            prevNode.next = newNode
+            newNode.next = nextNode
+            this.length++
+            return true
+        }
+    }
+    remove(index){
+        if(index < 0 || index >= this.length){
+            return undefined
+        }
+        else if(index === 0){
+            return this.shift()
+        }
+        else if(index === this.length - 1){
+            return this.pop()
+        }
+        else{
+            let prevNode = this.get(index-1)
+            let removed = prevNode.next
+            prevNode.next = removed.next
+            this.length--
+            return removed
+        }
+    }
+    reverse(){
+        let node = this.head
+        this.head = this.tail
+        this.tail = node
+        let next
+        let prev = null
+        for (let i = 0; i < this.length; i++) {
+            next = node.next 
+            node.next = prev 
+            prev = node
+            node = next 
+        }
+        return this
+    }
+    print(){
+        var arr = []
+        var current = this.head
+        while (current) {
+            arr.push(current.val)
+            current = current.next
+        }
+        console.log(arr)
+    }
+}
+ 
+var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+list.push("all")
+list.push("you")
+list.push("people")
+list.reverse()
+// list.remove(1)
+list.print()
+
+// list.insert(2, "Noon")
+// console.log("get",list.get(0))
+// console.dir(list, {depth: null})
+
+//HELLO -> null
+
+//  ["HELLO", "GOODBYE", "all", "you", "people"]
+//    node     
+
+//               node
+//    prev
